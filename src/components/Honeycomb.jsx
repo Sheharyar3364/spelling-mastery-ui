666f33fcf0c7aa8819e902e9a116739bf4533bb7
Muhammad Sheharyar <muhammadsheharyar3364@gmail.com>
2024-08-22 12:12:54 +0500

    removed dark classes

--- src/components/Honeycomb.jsx
+++ src/components/Honeycomb.jsx
@@ -158,7 +158,7 @@ export function Honeycomb( {showConfetti, setShowConfetti, setLoading } ) {
                     </button>
                 </>
                     :
-                    <button onClick={handleNextGame} className="next-btn text-white inline-flex w-full justify-center hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <button onClick={handleNextGame} className="next-btn text-white inline-flex w-full justify-center hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                         Next Game
                     </button>
                 }
